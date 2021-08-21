20daf98d17d923a7d26f786a5303c8cf4e32c463
Archit26k <76521139+Archit26k@users.noreply.github.com>
2021-08-21 15:37:17 +0530

    Update Security.js

--- js/Security.js
+++ js/Security.js
@@ -10,7 +10,7 @@ class Security {
         this.button1.position(100,120);
         this.button1.style('background', 'lightgrey');    
 
-        this.access2 = createInput("Code1")
+        this.access2 = createInput("Code2")
         this.access2.position(700,190);
         this.access2.style('background', 'white');  
 
@@ -18,7 +18,7 @@ class Security {
         this.button2.position(700,220);
         this.button2.style('background', 'lightgrey');
 
-        this.access3 = createInput("Code1")
+        this.access3 = createInput("Code3")
         this.access3.position(100,290);
         this.access3.style('background', 'white');  
 
